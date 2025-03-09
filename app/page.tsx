152d69ed3e236d3be662b05d4233e2b60d185494
dimichiko <ianvamvoukas1230@gmail.com>
2025-03-09 18:18:05 -0300

    Image

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from './ui/home.module.css'; // Importa el módulo CSS
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -28,9 +29,23 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <img
+          {/* <img
               src='/hero-desktop.png'
               alt="Screenshots of the dashboard"
+              /> */}
+              <Image
+              src='/hero-desktop.png'
+              alt="Screenshots of the dashboard"
+              width={1000}
+              height={760}
+              className='hidden md:block'
+              />
+              <Image
+              src='/hero-mobile.png'
+              alt="Screenshots of the dashboard"
+              width={560}
+              height={620}
+              className='block md:hidden'
               />
         </div>
       </div>
