cd28d387f67f011a8485fd62136ea0527ad9c919
dimichiko <ianvamvoukas1230@gmail.com>
2025-03-09 13:54:19 -0300

    x

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Asegúrate de incluir esta línea
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
