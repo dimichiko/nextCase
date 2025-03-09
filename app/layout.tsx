56fa720d7ed0568afecbfe6450a5b3890f34114b
dimichiko <ianvamvoukas1230@gmail.com>
2025-03-09 18:36:50 -0300

    Rutas/

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import { Montserrat } from 'next/font/google';
 import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={'${montserrat.className} antialiased'}>
+        Esto es parte del layout
+        {children}
+        <footer className='py-10 flex justify-center items-center'> 
+        De nada, con cariño de Next.js
+        </footer>
+      </body>
     </html>
   );
 }
